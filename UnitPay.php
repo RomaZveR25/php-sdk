c3f45c4659da9a05ae582c18cda33af5a01422fa
Roman <zversoftware@gmail.com>
2018-07-28 15:08:42 +1000

    Fixed CURL SSL verification

--- UnitPay.php
+++ UnitPay.php
@@ -130,8 +130,8 @@ class UnitPay
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); 
-		curl_setopt($ch, CURLOPT_TIMEOUT, 15); 
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_URL, $url);
 		$response = curl_exec($ch);
